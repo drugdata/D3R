5b3bbc2b7475b473bf418a3799720bb532a36c67
Christopher Churas <churas@gmail.com>
2015-06-15 15:50:56 -0700

    added _find_latest_weekly_dataset and _find_latest_year functions

--- d3r/task.py
+++ d3r/task.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import os
+import logging
+import re
+
+from os import listdir
+
+logger = logging.getLogger(__name__)
+
+class D3RParameters(object):
+    pass
+
 class D3RTask:
    """Represents a base Task that can be run.
    
@@ -9,11 +20,12 @@ class D3RTask:
 
    """
 
-   def __init__(self):
+   def __init__(self,args):
        self._name = "hi"
        self._stage = 1
        self._status = "unknown"
        self._error = ""
+       self._args = args
 
    def get_name(self):
        return self._name
@@ -53,8 +65,78 @@ class BlastNFilterTask(D3RTask):
 
 
    """
-   def __init__(self):
-       D3RTask.__init__(self)
+
+   def __init__(self,args):
+       D3RTask.__init__(self,args)
 
    def run(self):
-       print "Hello me"
+       print "BlastNFilterTask"
+       logger.debug("In BlastNFilter Task")
+
+
+def _find_latest_year(celppdir):
+    """Given a directory find the latest year
+  
+       The latest year will be a folder of 4 digits
+       and have the highest value ie 2015
+       :return: Directory or None if none is found
+       :raises: Exception: If celppdir is not a directory
+       """
+
+    if not os.path.isdir(celppdir):
+        raise Exception(celppdir+" is not a directory")
+
+    dirPattern = re.compile("^\d\d\d\d$")
+
+    latestYear = -1
+    latestEntry = None
+    fullPath = None
+    for entry in os.listdir(celppdir):
+        if re.match(dirPattern,entry):
+            entryYear = int(entry)
+            
+            if entryYear > latestYear:
+                fullPath = os.path.join(celppdir,entry)
+                if os.path.isdir(fullPath):
+                    latestYear = entryYear
+                    latestEntry = fullPath
+                
+    if latestYear == -1:
+        return
+
+    return latestEntry
+    
+
+def _find_latest_weekly_dataset(celppdir):
+    """Given a directory find the latest dataset
+  
+       This method looks in directory passed in for
+       paths with dataset.week.# format and returns
+       the path with highest # on it.  If none are
+       found then None is returned.
+       :return: Directory upon success or None if no directory found
+       :raises: Exception: If celppdir is not a directory
+       """
+
+    latestYear = _find_latest_year(celppdir)
+
+    if latestYear is None:
+        return
+
+    celppdirWithYear = os.path.join(celppdir,latestYear)
+
+    dirPattern = re.compile("^dataset.week.\d+$")
+
+    latestEntry = None
+    latestWeekNo = -1
+    for entry in os.listdir(celppdirWithYear):
+        if re.match(dirPattern,entry):
+           weekNo = re.sub("dataset.week.","",entry)
+           if weekNo > latestWeekNo:
+               latestWeekNo = weekNo
+               latestEntry = entry
+
+    if latestWeekNo == -1:
+        return
+  
+    return os.path.join(celppdirWithYear,latestEntry)
