6dbf5ca6aaccd4589dd1a4acf8eda3c0cb48753b
Christopher Churas <churas@gmail.com>
2015-06-15 15:49:37 -0700

    added test for find_latest_year

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+import unittest
+import tempfile
+import logging
+import os.path
+
+import d3r.task
+
 """
 test_task
 ----------------------------------
@@ -18,9 +26,40 @@ class TestD3rTask(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_find_latest_year(self):
+        tempDir = tempfile.mkdtemp()
 
+	
+        self.assertIsNone(task._find_latest_year(tempDir))
+       
+        os.mkdir(os.path.join(tempDir,"foo"))
+        os.mkdir(os.path.join(tempDir,"2014"))
+        self.assertEqual(task._find_latest_year(tempDir),
+                         os.path.join(tempDir,'2014')) 
+
+        open(os.path.join(tempDir,'2016'),'a').close()
+        self.assertEqual(task._find_latest_year(tempDir),  
+                         os.path.join(tempDir,'2014'))
+
+        os.mkdir(os.path.join(tempDir,'2012'))
+           
+        self.assertEqual(task._find_latest_year(tempDir),
+                         os.path.join(tempDir,'2014'))
+
+        os.mkdir(os.path.join(tempDir,'2015'))
+
+
+        self.assertEqual(task._find_latest_year(tempDir),
+                         os.path.join(tempDir,'2015'))
+
+        os.rmdir(os.path.join(tempDir,"foo"))
+        os.rmdir(os.path.join(tempDir,"2012"))
+        os.rmdir(os.path.join(tempDir,"2014"))
+        os.rmdir(os.path.join(tempDir,"2015"))
+        os.remove(os.path.join(tempDir,'2016'))
+        
+        os.rmdir(tempDir)
+     
     def tearDown(self):
         pass
 
