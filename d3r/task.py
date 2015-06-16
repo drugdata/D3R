93534f77b423e5b076123c91324ad29971130be5
Christopher Churas <churas@gmail.com>
2015-06-16 14:45:05 -0700

    fixes for lint and added tests and implementation so _find_latest_weekly_dataset and _find_latest_year work right

--- d3r/task.py
+++ d3r/task.py
@@ -4,79 +4,79 @@ import os
 import logging
 import re
 
-from os import listdir
-
 logger = logging.getLogger(__name__)
+
 
 class D3RParameters(object):
     pass
 
+
 class D3RTask:
-   """Represents a base Task that can be run.
-   
-   This is a base class from which other classes that actual do work
-   can be derived from.  It provides internal attributes for name,
-   stage, and status.  As well as a run() function.  
+    """Represents a base Task that can be run.
 
-   """
+    This is a base class from which other classes that actual do work
+    can be derived from.  It provides internal attributes for name,
+    stage, and status.  As well as a run() function.
 
-   def __init__(self,args):
-       self._name = "hi"
-       self._stage = 1
-       self._status = "unknown"
-       self._error = ""
-       self._args = args
+    """
 
-   def get_name(self):
-       return self._name
+    def __init__(self, args):
+        self._name = "hi"
+        self._stage = 1
+        self._status = "unknown"
+        self._error = ""
+        self._args = args
 
-   def set_name(self,name):
-       self._name = name
+    def get_name(self):
+        return self._name
 
-   def get_stage(self):
-       return self._stage
+    def set_name(self, name):
+        self._name = name
 
-   def set_stage(self,stage):
-       self._stage = stage
+    def get_stage(self):
+        return self._stage
 
-   def get_status(self):
-       return self._status
+    def set_stage(self, stage):
+        self._stage = stage
 
-   def set_status(self,status):
-       self._status = status
+    def get_status(self):
+        return self._status
 
-   def get_error(self):
-       return self._error
+    def set_status(self, status):
+        self._status = status
 
-   def set_error(self,error):
-       self._error = error
+    def get_error(self):
+        return self._error
 
-   def start(self):
-       print "Task ",self._name," is starting"
+    def set_error(self, error):
+        self._error = error
 
-   def end(self):
-       print "Task ",self._name," has finished"
+    def start(self):
+        print "Task ", self._name, " is starting"
 
-   def run(self):
-       print "Running ",self._name
+    def end(self):
+        print "Task ", self._name, " has finished"
+
+    def run(self):
+        print "Running ", self._name
+
 
 class BlastNFilterTask(D3RTask):
-   """Performs Blast and filter of sequences
+    """Performs Blast and filter of sequences
 
+    """
 
-   """
+    def __init__(self, args):
+        D3RTask.__init__(self, args)
 
-   def __init__(self,args):
-       D3RTask.__init__(self,args)
-
-   def run(self):
-       print "BlastNFilterTask"
-       logger.debug("In BlastNFilter Task")
+    def run(self):
+        print "BlastNFilterTask"
+        logger.debug("In BlastNFilter Task")
 
 
 def _find_latest_year(celppdir):
     """Given a directory find the latest year
-  
+
        The latest year will be a folder of 4 digits
        and have the highest value ie 2015
        :return: Directory or None if none is found
@@ -92,24 +92,24 @@ def _find_latest_year(celppdir):
     latestEntry = None
     fullPath = None
     for entry in os.listdir(celppdir):
-        if re.match(dirPattern,entry):
+        if re.match(dirPattern, entry):
             entryYear = int(entry)
-            
+
             if entryYear > latestYear:
-                fullPath = os.path.join(celppdir,entry)
+                fullPath = os.path.join(celppdir, entry)
                 if os.path.isdir(fullPath):
                     latestYear = entryYear
                     latestEntry = fullPath
-                
+
     if latestYear == -1:
         return
 
     return latestEntry
-    
+
 
 def _find_latest_weekly_dataset(celppdir):
     """Given a directory find the latest dataset
-  
+
        This method looks in directory passed in for
        paths with dataset.week.# format and returns
        the path with highest # on it.  If none are
@@ -123,20 +123,20 @@ def _find_latest_weekly_dataset(celppdir):
     if latestYear is None:
         return
 
-    celppdirWithYear = os.path.join(celppdir,latestYear)
+    celppdirWithYear = os.path.join(celppdir, latestYear)
 
     dirPattern = re.compile("^dataset.week.\d+$")
 
     latestEntry = None
     latestWeekNo = -1
+    fullPath = None
     for entry in os.listdir(celppdirWithYear):
-        if re.match(dirPattern,entry):
-           weekNo = re.sub("dataset.week.","",entry)
-           if weekNo > latestWeekNo:
-               latestWeekNo = weekNo
-               latestEntry = entry
+        if re.match(dirPattern, entry):
+            weekNo = re.sub("dataset.week.", "", entry)
+            if weekNo > latestWeekNo:
+                fullPath = os.path.join(celppdirWithYear, entry)
+                if os.path.isdir(fullPath):
+                    latestWeekNo = weekNo
+                    latestEntry = fullPath
 
-    if latestWeekNo == -1:
-        return
-  
-    return os.path.join(celppdirWithYear,latestEntry)
+    return latestEntry
--- tests/test_task.py
+++ tests/test_task.py
@@ -4,10 +4,7 @@
 
 import unittest
 import tempfile
-import logging
 import os.path
-
-import d3r.task
 
 """
 test_task
@@ -16,7 +13,6 @@ test_task
 Tests for `task` module.
 """
 
-import unittest
 
 from d3r import task
 
@@ -28,38 +24,57 @@ class TestD3rTask(unittest.TestCase):
 
     def test_find_latest_year(self):
         tempDir = tempfile.mkdtemp()
+        self.assertEqual(task._find_latest_year(tempDir), None)
 
-	
-        self.assertEqual(task._find_latest_year(tempDir),None)
-       
-        os.mkdir(os.path.join(tempDir,"foo"))
-        os.mkdir(os.path.join(tempDir,"2014"))
+        os.mkdir(os.path.join(tempDir, "foo"))
+        os.mkdir(os.path.join(tempDir, "2014"))
         self.assertEqual(task._find_latest_year(tempDir),
-                         os.path.join(tempDir,'2014')) 
+                         os.path.join(tempDir, '2014'))
 
-        open(os.path.join(tempDir,'2016'),'a').close()
-        self.assertEqual(task._find_latest_year(tempDir),  
-                         os.path.join(tempDir,'2014'))
-
-        os.mkdir(os.path.join(tempDir,'2012'))
-           
+        open(os.path.join(tempDir, '2016'), 'a').close()
         self.assertEqual(task._find_latest_year(tempDir),
-                         os.path.join(tempDir,'2014'))
+                         os.path.join(tempDir, '2014'))
 
-        os.mkdir(os.path.join(tempDir,'2015'))
+        os.mkdir(os.path.join(tempDir, '2012'))
+        self.assertEqual(task._find_latest_year(tempDir),
+                         os.path.join(tempDir, '2014'))
 
+        os.mkdir(os.path.join(tempDir, '2015'))
 
         self.assertEqual(task._find_latest_year(tempDir),
-                         os.path.join(tempDir,'2015'))
+                         os.path.join(tempDir, '2015'))
 
-        os.rmdir(os.path.join(tempDir,"foo"))
-        os.rmdir(os.path.join(tempDir,"2012"))
-        os.rmdir(os.path.join(tempDir,"2014"))
-        os.rmdir(os.path.join(tempDir,"2015"))
-        os.remove(os.path.join(tempDir,'2016'))
-        
+        os.rmdir(os.path.join(tempDir, "foo"))
+        os.rmdir(os.path.join(tempDir, "2012"))
+        os.rmdir(os.path.join(tempDir, "2014"))
+        os.rmdir(os.path.join(tempDir, "2015"))
+        os.remove(os.path.join(tempDir, '2016'))
         os.rmdir(tempDir)
-     
+
+    def test_find_latest_weekly_dataset(self):
+        tempDir = tempfile.mkdtemp()
+
+        self.assertEqual(task._find_latest_weekly_dataset(tempDir), None)
+
+        os.mkdir(os.path.join(tempDir, '2015'))
+
+        self.assertEqual(task._find_latest_weekly_dataset(tempDir), None)
+
+        open(os.path.join(tempDir, '2015', 'dataset.week.4'), 'a').close()
+
+        self.assertEqual(task._find_latest_weekly_dataset(tempDir), None)
+
+        os.mkdir(os.path.join(tempDir, '2015', 'dataset.week.3'))
+
+        self.assertEqual(task._find_latest_weekly_dataset(tempDir),
+                         os.path.join(tempDir, '2015',
+                                      'dataset.week.3'))
+
+        os.rmdir(os.path.join(tempDir, "2015", "dataset.week.3"))
+        os.remove(os.path.join(tempDir, "2015", "dataset.week.4"))
+        os.rmdir(os.path.join(tempDir, "2015"))
+        os.rmdir(tempDir)
+
     def tearDown(self):
         pass
 
