b8819fdd696425d2365d040be51fbd0b204a3472
Christopher Churas <churas@gmail.com>
2015-10-05 19:08:17 -0700

    added get_celpp_week_of_year_from_date and create_celpp_week_dir functions

--- d3r/util.py
+++ d3r/util.py
@@ -3,7 +3,12 @@
 import os
 import logging
 import re
+from datetime import date
+from datetime import timedelta
+
 logger = logging.getLogger(__name__)
+
+DAYS_IN_WEEK = 7
 
 
 def find_latest_year(celppdir):
@@ -16,7 +21,7 @@ def find_latest_year(celppdir):
        """
 
     if not os.path.isdir(celppdir):
-        raise Exception(celppdir+" is not a directory")
+        raise Exception(celppdir + " is not a directory")
 
     dir_pattern = re.compile("^\d\d\d\d$")
 
@@ -70,3 +75,54 @@ def find_latest_weekly_dataset(celppdir):
                     latest_entry = full_path
 
     return latest_entry
+
+
+def get_celpp_week_of_year_from_date(the_date):
+    """Returns CELPP week of year
+    The CELPP week of year matches ISO week of year, but it
+    starts on Friday instead of Monday.  The way it works is
+    the code examines `the_date` and if its 12:00am Monday -
+    11:59pm Thursday the current week according to date.isocalendar()
+    is used.  If date falls between Friday 12:00am to Sunday 11:59pm
+    this function advances the date to the following Monday and extracts
+    week of year.
+       :return: tuple with first value the week and second value the year
+       :raises: Exception if `the_date` is None
+    """
+    if the_date is None:
+        raise Exception('Must pass a valid date')
+    assert isinstance(the_date, date)
+    updated_date = the_date
+
+    day_of_week = the_date.weekday()
+    if day_of_week >= 4:
+        updated_date = the_date + timedelta(days=(DAYS_IN_WEEK - day_of_week))
+        logger.debug('Date ' + str(the_date) + ' is between Friday and ' +
+                     'Sunday shifting date to ' + str(updated_date))
+
+    return (updated_date.isocalendar()[1],
+            updated_date.isocalendar()[0])
+
+def create_celpp_week_dir(celpp_week_tuple, celppdir):
+    """Creates celpp week dir in `celppdir`
+    Creates celpp week directory under `celppdir`
+    using `celpp_week_tuple` to get year and week of year
+    diretories created:
+    celppdir/<celpp_week_tuple[1]>/dataset.week.<celpp_week_tuple[0]>
+    If directory already exists no operation is performed.
+    :raises: OSError if unable to create directory
+    """
+    if celpp_week_tuple is None:
+        raise Exception('celpp week tuple must be set')
+
+    if celppdir is None:
+        raise Exception('celppdir must be set')
+
+    dir_to_create = os.path.join(celppdir, str(celpp_week_tuple[1]),
+                                 'dataset.week.'+str(celpp_week_tuple[0]))
+    if os.path.isdir(dir_to_create):
+        logger.debug(dir_to_create + ' directory already exists skipping creation')
+        return
+
+    logger.debug('Creating ' + dir_to_create)
+    os.makedirs(dir_to_create, 0775)
--- tests/test_util.py
+++ tests/test_util.py
@@ -14,7 +14,7 @@ Tests for `task` module.
 """
 
 import shutil
-
+from datetime import date
 from d3r import util
 
 
@@ -77,6 +77,92 @@ class TestUtil(unittest.TestCase):
                                           'dataset.week.3'))
         finally:
             shutil.rmtree(temp_dir)
+
+    def test_get_celpp_week_of_year(self):
+        # try passing none
+        try:
+            util.get_celpp_week_of_year_from_date(None)
+            self.fail('Expected exception')
+        except Exception:
+            pass
+
+        #try 1-1-2016
+        celp_week = util.get_celpp_week_of_year_from_date(date(2016,1,1))
+        self.assertEqual(celp_week[0],1)
+        self.assertEqual(celp_week[1],2016)
+
+        #try 12-31-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,12,31))
+        self.assertEqual(celp_week[0],53)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-5-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,5))
+        self.assertEqual(celp_week[0],41)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-8-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,8))
+        self.assertEqual(celp_week[0],41)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-9-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,9))
+        self.assertEqual(celp_week[0],42)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-10-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,10))
+        self.assertEqual(celp_week[0],42)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-11-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,11))
+        self.assertEqual(celp_week[0],42)
+        self.assertEqual(celp_week[1],2015)
+
+        #try 10-12-2015
+        celp_week = util.get_celpp_week_of_year_from_date(date(2015,10,12))
+        self.assertEqual(celp_week[0],42)
+        self.assertEqual(celp_week[1],2015)
+
+    def test_create_celpp_week_dir(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+
+            try:
+                # try both parameters None
+                util.create_celpp_week_dir(None, None)
+
+                # try week tuple None
+                util.create_celpp_week_dir(None, temp_dir)
+
+                # try celppdir None
+                util.create_celpp_week_dir((1,2015), None)
+
+                self.fail('Expected exception')
+            except Exception:
+                pass
+
+            # try path already exists as dir
+            os.makedirs(os.path.join(temp_dir,'2015','dataset.week.1'), 0775)
+            util.create_celpp_week_dir((1,2015),temp_dir)
+
+            # try path already exists as file so it will fail
+            open(os.path.join(temp_dir,'2015','dataset.week.2'),'a').close()
+            try:
+                util.create_celpp_week_dir((2,2015),temp_dir)
+                self.fail('Expected OSError')
+            except OSError:
+                pass
+
+            # try path successful create
+            util.create_celpp_week_dir((3,2015),temp_dir)
+            new_dir = os.path.join(temp_dir,'2015','dataset.week.3')
+            self.assertEquals(os.path.isdir(new_dir), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
 
     def tearDown(self):
         pass
