8a65116880af4141e95e56f6a557094dfe0d6f5e
Christopher Churas <churas@gmail.com>
2015-10-07 16:44:50 -0700

    added can_run() and run() implementations which now download 4 files including compinchi file from ComponentInchi task that was removed

--- tests/celpp/test_dataimport.py
+++ tests/celpp/test_dataimport.py
@@ -5,6 +5,9 @@ __author__ = 'churas'
 
 
 import unittest
+import os
+import tempfile
+import shutil
 
 """
 test_dataimport
@@ -14,7 +17,9 @@ Tests for `dataimport` module.
 """
 
 from d3r.celpp.task import D3RParameters
+from d3r.celpp.task import D3RTask
 from d3r.celpp.dataimport import DataImportTask
+
 
 from tests.celpp import test_task
 
@@ -48,6 +53,188 @@ class TestDataImportTask(unittest.TestCase):
         self.assertEqual(task.get_crystalph_tsv(),
                          '/foo/stage.1.dataimport/' +
                          'new_release_crystallization_pH.tsv')
+
+    def test_can_run_with_complete_file(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            task = DataImportTask(temp_dir, params)
+            task.create_dir()
+            open(os.path.join(task.get_dir(),
+                              D3RTask.COMPLETE_FILE), 'a').close()
+            self.assertEquals(task.can_run(), False)
+            self.assertEquals(task.get_error(), None)
+            self.assertEquals(task._can_run, False)
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_can_run_does_not_exist_or_error(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            task = DataImportTask(temp_dir, params)
+
+            self.assertEquals(task.can_run(), True)
+            self.assertEquals(task.get_error(), None)
+            self.assertEquals(task._can_run, True)
+
+            task.create_dir()
+            open(os.path.join(task.get_dir(),
+                              D3RTask.ERROR_FILE), 'a').close()
+            self.assertEquals(task.can_run(), False)
+            self.assertEquals(task._can_run, False)
+            self.assertEquals(task.get_error(),
+                              'stage.1.dataimport already exists and ' +
+                              'status is ' + D3RTask.ERROR_STATUS)
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_but_can_run_flag_is_false(self):
+        params = D3RParameters()
+        task = DataImportTask('/foo', params)
+        task._can_run = False
+        task.run()
+
+    def test_run_pdbfileurl_not_set(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            task.run()
+            self.assertEquals(task.get_error(), 'cannot download files' +
+                              ' cause pdbfileurl not set')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_can_run_flag_false(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+            task = DataImportTask(temp_dir, params)
+            task._can_run = False
+            task.run()
+            self.assertEquals(task.get_error(), None)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_compinchi_not_set(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            task.run()
+            self.assertEquals(task.get_error(), 'cannot download files' +
+                              ' cause compinchi not set')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_all_nonpolymer_fail(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            task.run()
+            self.assertEquals(task.get_error(), 'Unable to download file ' +
+                              'from ' + params.pdbfileurl + ' to ' +
+                              task.get_nonpolymer_tsv())
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_all_sequence_fail(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            open(os.path.join(temp_dir,
+                              task.NONPOLYMER_TSV), 'a').close()
+            task.run()
+            self.assertEquals(task.get_error(), 'Unable to download file ' +
+                              'from ' + params.pdbfileurl + ' to ' +
+                              task.get_sequence_tsv())
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_all_crystalph_fail(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            open(os.path.join(temp_dir,
+                              task.NONPOLYMER_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.SEQUENCE_TSV), 'a').close()
+            task.run()
+            self.assertEquals(task.get_error(), 'Unable to download file ' +
+                              'from ' + params.pdbfileurl + ' to ' +
+                              task.get_crystalph_tsv())
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_all_compinchi_fail(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            open(os.path.join(temp_dir,
+                              task.NONPOLYMER_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.SEQUENCE_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.CRYSTALPH_TSV), 'a').close()
+            task.run()
+            self.assertEquals(task.get_error(), 'Unable to download file ' +
+                              'from ' + params.compinchi + ' to ' +
+                              task.get_components_inchi_file())
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_all_success(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.pdbfileurl = 'file://' + temp_dir
+            params.compinchi = 'file://' + temp_dir
+
+            task = DataImportTask(temp_dir, params)
+            task._retrysleep = 0
+            open(os.path.join(temp_dir,
+                              task.NONPOLYMER_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.SEQUENCE_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.CRYSTALPH_TSV), 'a').close()
+            open(os.path.join(temp_dir,
+                              task.COMPINCHI_ICH), 'a').close()
+
+            task.run()
+            self.assertEquals(task.get_error(), None)
+        finally:
+            shutil.rmtree(temp_dir)
 
     def tearDown(self):
         pass
