e5eecae03805f9c7dbd322daff1f6fb26430bf13
Chris Churas <churas@gmail.com>
2015-12-03 19:06:51 -0600

    partial coding of ScoringTask and ScoringTaskFactory

--- d3r/celpp/scoring.py
+++ d3r/celpp/scoring.py
@@ -8,17 +8,78 @@ from d3r.celpp.proteinligprep import ProteinLigPrepTask
 
 logger = logging.getLogger(__name__)
 
+class PathNotDirectoryError(Exception):
+    """Path is not a directory Error
+    """
+    pass
+
+class ScoringTaskFactory(object):
+    """Factory class to generate ScoringTask objects
+
+       This factory examines a celpp week directory for
+       all stage 4 tasks that are potential docking
+       tasks.  The code then generates ScoringTask
+       objects for all eligible docking tasks
+    """
+
+    def __init__(self, path, theargs):
+        """Constructor
+        """
+        self.set_path(path)
+        self.set_args(theargs)
+
+    def set_args(self, theargs):
+        self._args = theargs
+
+    def get_args(self):
+        return self._args
+
+    def set_path(self, path):
+        """Sets path used to look for docking tasks
+        """
+        self._path = path
+
+    def get_path(self):
+        """Gets path used to look for docking tasks
+        """
+        return self._path
+
+    def get_scoring_tasks(self):
+        """Generate ScoringTasks
+
+           This method creates a list of
+           ScoringTask objects, one for each
+           stage 4 dock task found in path set
+           via setpath() or via constructor of
+           this object
+        """
+        path = self.get_path()
+        if not os.path.isdir(path):
+            raise PathNotDirectoryError(path + ' is not a directory')
+        scoring_tasks = []
+        path_list = os.listdir(path)
+
+        for entry in path_list:
+            full_path = os.path.join(path, entry)
+            if os.path.isdir(full_path):
+                if entry.startswith('stage.4.'):
+                    if not entry.endswith('webdata'):
+                        # we have a valid docking path
+                        pass #NEED TO CALL Scoring task constructor here
+        return scoring_tasks
+
 
 class ScoringTask(D3RTask):
     """Performs Scoring
 
     """
 
-    def __init__(self, path, args):
+    def __init__(self, path, name, docktask, args):
         super(ScoringTask, self).__init__(path, args)
-        self.set_name('glide')
+        self.set_name(name)
         self.set_stage(4)
         self.set_status(D3RTask.UNKNOWN_STATUS)
+        self._docktask = docktask
 
     def can_run(self):
         """Determines if task can actually run
@@ -32,15 +93,13 @@ class ScoringTask(D3RTask):
         """
         self._can_run = False
         self._error = None
-        # check blast
-        blastnfilter = ProteinLigPrepTask(self._path, self._args)
-        blastnfilter.update_status_from_filesystem()
-        if blastnfilter.get_status() != D3RTask.COMPLETE_STATUS:
+        self._docktask.update_status_from_filesystem()
+        if self._docktask.get_status() != D3RTask.COMPLETE_STATUS:
             logger.info('Cannot run ' + self.get_name() + 'task ' +
-                        'because ' + blastnfilter.get_name() + 'task' +
-                        'has a status of ' + blastnfilter.get_status())
-            self.set_error(blastnfilter.get_name() + ' task has ' +
-                           blastnfilter.get_status() + ' status')
+                        'because ' + self._docktask.get_name() + 'task' +
+                        'has a status of ' + self._docktask.get_status())
+            self.set_error(self._docktask.get_name() + ' task has ' +
+                           self._docktask.get_status() + ' status')
             return False
 
         # check this task is not complete and does not exist
@@ -92,7 +151,7 @@ class ScoringTask(D3RTask):
         #
         cmd_to_run = (self.get_args().scoring + ' --pdbdb ' +
                       self.get_args().pdbdb + ' --dockdir ' +
-                      docktask.get_dir() +
+                      self._docktask.get_dir() +
                       ' --outdir ' + self.get_dir())
 
         scoring_name = os.path.basename(self.get_args().scoring)
