a3054eee8817cd2305c629ee3a32d0e7fe2ab68d
shuail <shuailiu25@gmail.com>
2015-12-11 11:30:43 -0800

    update the proprep and docking scripts with new parameter options

--- d3r/glidedocking.py
+++ d3r/glidedocking.py
@@ -52,7 +52,7 @@ def main_glide (stage_3_result, stage_4_working):
         if all_pdb_path[0] != stage_3_result:
             dockable_paths.append(all_pdb_path[0])
 
-    for dockable_path in dockable_paths[:5]:
+    for dockable_path in dockable_paths:
         commands.getoutput("cp -r %s %s"%(dockable_path, stage_4_working))
         dockable_path_local = os.path.basename(dockable_path)
         os.chdir(dockable_path_local)
@@ -61,13 +61,16 @@ def main_glide (stage_3_result, stage_4_working):
         #Do the docking here
         #1, get the center
         grid_center = open("center.txt","r").readlines()[0]
-        print "GGGGGGGGGGG", grid_center 
-        #for possible_protein in ("largest.maegz", "smallest.maegz", "holo.maegz", "apo.maegz"):
-        for possible_protein in ("smallest.maegz", "holo.maegz", "apo.maegz"):
+        if grid_center:
+            
+            logging.info("=============Start working in this case:%s=========="%dockable_path_local)
+        else:
+            logging.info("Fatal error: Unable to find the center file for this case %s"%dockable_path_local)
+            continue
+        for possible_protein in ("largest.maegz", "smallest.maegz", "holo.maegz", "apo.maegz"):
+        #for possible_protein in ("smallest.maegz", "holo.maegz", "apo.maegz"):
             if os.path.isfile(possible_protein):
-                print "Working in this pdb: %s"%possible_protein
-                raw_input()
-                logging.info("Working in this pdb: %s"%possible_protein)
+                logging.info( "Working in this pdb: %s"%possible_protein )
                 protein_title = possible_protein.split(".")[0]
                 if not os.path.isdir(protein_title):
                     os.mkdir(protein_title)
@@ -78,9 +81,11 @@ def main_glide (stage_3_result, stage_4_working):
                 commands.getoutput("cp ../ligand.mae .")
                 #generate grid
                 out_grid = grid(grid_center, possible_protein) 
-                print "WWWWWWWWWWWWWWWWW"
+                logging.info("Trying to get the grid file...")
                 time.sleep(200)
+                logging.info("Finish grid generation...")
                 dock("ligand.mae", out_grid, protein_title)
+                logging.info("Finish docking...")
                 ##################
                 os.chdir(current_dir_layer_2)
         os.chdir(current_dir)
@@ -90,16 +95,16 @@ def main_glide (stage_3_result, stage_4_working):
 if ("__main__") == (__name__):
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("-p", "--pdbloc", metavar = "PATH", help = "PDB DATABANK which we will dock into")
-    parser.add_option("-l", "--locs2", metavar="PATH", help = "PATH where we could find the stage 2 output")
-    parser.add_option("-w", "--wfolder", metavar = "PATH", help = "PATH where we run stage 3")
-    #parser.add_option("-s", "--sleep", metavar = "VALUE", help = "Sleep time for protein prep")
-    #parser.add_option("-u", "--update", default = False, action = "store_true", help = "update the protein generation and docking step")
+    parser.add_option("-s", "--structuredir", metavar="PATH", help = "PATH where we could find the stage 2 output")
+    parser.add_option("-o", "--outdir", metavar = "PATH", help = "PATH where we run stage 3")
     logger = logging.getLogger()
     logging.basicConfig( format  = '%(asctime)s: %(message)s', datefmt = '%m/%d/%y %I:%M:%S', filename = 'final.log', filemode = 'w', level   = logging.INFO )
     (opt, args) = parser.parse_args()
-    #pdb_location = opt.pdbloc
-    stage_3_result = opt.locs2
-    stage_4_result = opt.wfolder
-    #sleep_time = opt.sleep
+    stage_3_result = opt.structuredir
+    stage_4_result = opt.outdir
+    #running under this dir
+    running_dir = os.getcwd()
     main_glide(stage_3_result, stage_4_result)
+    #move the final log file to the result dir
+    log_file_path = os.path.join(running_dir, 'final.log')
+    commands.getoutput("mv %s %s"%(log_file_path, stage_4_result))
