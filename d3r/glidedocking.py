03da4b8711b17dccea79fa5dc3c9ea16d062288d
shuail <shuailiu25@gmail.com>
2015-12-17 15:09:04 -0800

    modify glidedocking.py to add features to skip the cases without center.txt and enable the updating method

--- d3r/glidedocking.py
+++ d3r/glidedocking.py
@@ -44,7 +44,8 @@ def dock(ligand_file, out_grid, protein_title):
     f.close()
     out_dock_file = protein_title + "_dock_pv.maegz"
     commands.getoutput("$SCHRODINGER/glide -WAIT %s"%dock_in)
-def main_glide (stage_3_result, stage_4_working):
+    return out_dock_file
+def main_glide (stage_3_result, stage_4_working, update= True):
     os.chdir(stage_4_working)
     current_dir = os.getcwd()
     dockable_paths = []
@@ -60,12 +61,12 @@ def main_glide (stage_3_result, stage_4_working):
         ##################
         #Do the docking here
         #1, get the center
-        grid_center = open("center.txt","r").readlines()[0]
-        if grid_center:
-            
+        try:
+            grid_center = open("center.txt","r").readlines()[0]
             logging.info("=============Start working in this case:%s=========="%dockable_path_local)
-        else:
+        except:
             logging.info("Fatal error: Unable to find the center file for this case %s"%dockable_path_local)
+            os.chdir(current_dir)
             continue
         for possible_protein in ("largest.maegz", "smallest.maegz", "holo.maegz", "apo.maegz"):
         #for possible_protein in ("smallest.maegz", "holo.maegz", "apo.maegz"):
@@ -80,11 +81,24 @@ def main_glide (stage_3_result, stage_4_working):
                 commands.getoutput("cp ../%s ."%possible_protein)
                 commands.getoutput("cp ../ligand.mae .")
                 #generate grid
-                out_grid = grid(grid_center, possible_protein) 
                 logging.info("Trying to get the grid file...")
-                time.sleep(200)
+                if update: 
+                    out_grid = grid(grid_center, possible_protein) 
+                    time.sleep(200)
+                else:
+                    #check if there is old grid
+                    out_grid = possible_protein.split(".")[0] + ".zip"
+                    if not os.path.isfile(out_grid):
+                        out_grid = grid(grid_center, possible_protein)
+                        time.sleep(200)
                 logging.info("Finish grid generation...")
-                dock("ligand.mae", out_grid, protein_title)
+                logging.info("Trying to dock...")
+                if update:
+                    out_dock = dock("ligand.mae", out_grid, protein_title)
+                else:
+                    out_dock = protein_title + "_dock_pv.maegz"
+                    if not os.path.isfile(out_dock):
+                        out_dock = dock("ligand.mae", out_grid, protein_title)
                 logging.info("Finish docking...")
                 ##################
                 os.chdir(current_dir_layer_2)
@@ -97,14 +111,16 @@ if ("__main__") == (__name__):
     parser = OptionParser()
     parser.add_option("-s", "--structuredir", metavar="PATH", help = "PATH where we could find the stage 2 output")
     parser.add_option("-o", "--outdir", metavar = "PATH", help = "PATH where we run stage 3")
+    parser.add_option("-u", "--update", action = "store_true",  help = "Update the docking result", default = False) 
     logger = logging.getLogger()
     logging.basicConfig( format  = '%(asctime)s: %(message)s', datefmt = '%m/%d/%y %I:%M:%S', filename = 'final.log', filemode = 'w', level   = logging.INFO )
     (opt, args) = parser.parse_args()
     stage_3_result = opt.structuredir
     stage_4_result = opt.outdir
+    update = opt.update
     #running under this dir
     running_dir = os.getcwd()
-    main_glide(stage_3_result, stage_4_result)
+    main_glide(stage_3_result, stage_4_result, update = update)
     #move the final log file to the result dir
     log_file_path = os.path.join(running_dir, 'final.log')
     commands.getoutput("mv %s %s"%(log_file_path, stage_4_result))
