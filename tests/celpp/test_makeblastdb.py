16099fa37c5dfca1a33ee3c545a6aae0e627baf4
Chris Churas <churas@gmail.com>
2016-02-03 16:29:14 -0800

    added comments denoting tests that need to be written

--- tests/celpp/test_makeblastdb.py
+++ tests/celpp/test_makeblastdb.py
@@ -16,13 +16,17 @@ Tests for `makeblastdb` module.
 from d3r.celpp.task import D3RParameters
 from d3r.celpp.task import D3RTask
 from d3r.celpp.makeblastdb import MakeBlastDBTask
-
 from tests.celpp import test_task
 
 
 class TestMakeBlastDBTask(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_update_status_from_filesystem(self):
+        params = D3RParameters()
+        task = MakeBlastDBTask(None, params)
+        test_task.try_update_status_from_filesystem(self, task)
 
     def test_constructor(self):
         params = D3RParameters()
@@ -33,6 +37,18 @@ class TestMakeBlastDBTask(unittest.TestCase):
         self.assertEqual(task.get_path(), '/foo')
         self.assertEqual(task.get_dir_name(), 'stage.1.makeblastdb')
         test_task.try_update_status_from_filesystem(self, task)
+
+# Test run where pdbsequrl is not set
+
+# Test run where makeblastdb is not set
+
+# Test run where url download fails
+
+# Test run where gunzip fails
+
+# Test run where makeblastdb fails
+
+# Test run where everythin is successful
 
     def tearDown(self):
         pass
