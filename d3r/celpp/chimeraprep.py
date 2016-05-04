78e4cce88ae2934bccdb809468a5e70a1d45e750
Chris Churas <churas@gmail.com>
2016-05-04 17:40:08 -0500

    initial revision

---
+++ d3r/celpp/chimeraprep.py
@@ -0,0 +1,140 @@
+__author__ = 'churas'
+
+import os
+import logging
+
+from d3r.celpp.task import D3RTask
+from d3r.celpp.challengedata import ChallengeDataTask
+
+logger = logging.getLogger(__name__)
+
+
+class ChimeraProteinLigPrepTask(D3RTask):
+    """Performs preparation of PDB and inchi files
+
+    """
+
+    FINAL_LOG = 'final.log'
+
+    def __init__(self, path, args):
+        super(ChimeraProteinLigPrepTask, self).__init__(path, args)
+        self.set_name('chimeraprep')
+
+        # Make stage number one higher then ChallengeDataTask Stage
+        task = ChallengeDataTask(path, args)
+        self.set_stage(task.get_stage() + 1)
+
+        self.set_status(D3RTask.UNKNOWN_STATUS)
+
+    def get_uploadable_files(self):
+        """Returns list of files that can be uploaded to remote server
+
+           List will contain these files in addition to stderr/stdout files
+           if they are found on the filesystem
+
+           final.log
+
+           :returns: list of files that can be uploaded.
+        """
+        # get the stderr/stdout files
+        file_list = super(ChimeraProteinLigPrepTask, self).get_uploadable_files()
+
+        out_dir = self.get_dir()
+
+        final_log = os.path.join(out_dir, ChimeraProteinLigPrepTask.FINAL_LOG)
+        if os.path.isfile(final_log):
+            file_list.append(final_log)
+
+        return file_list
+
+    def can_run(self):
+        """Determines if task can actually run
+
+           This method first verifies the `BlastNFilterTask` task
+           has `D3RTask.COMPLETE_STATUS` for
+           status.  The method then verifies a `ProteinLigPrepTask` does
+           not already exist.  If above is not true then self.set_error()
+           is set with information about the issue
+           :return: True if can run otherwise False
+        """
+        self._can_run = False
+        self._error = None
+        # check blast
+        blastnfilter = ChallengeDataTask(self._path, self._args)
+        blastnfilter.update_status_from_filesystem()
+        if blastnfilter.get_status() != D3RTask.COMPLETE_STATUS:
+            logger.info('Cannot run ' + self.get_name() + 'task ' +
+                        'because ' + blastnfilter.get_name() + 'task' +
+                        'has a status of ' + blastnfilter.get_status())
+            self.set_error(blastnfilter.get_name() + ' task has ' +
+                           blastnfilter.get_status() + ' status')
+            return False
+
+        # check this task is not complete and does not exist
+
+        self.update_status_from_filesystem()
+        if self.get_status() == D3RTask.COMPLETE_STATUS:
+            logger.debug("No work needed for " + self.get_name() +
+                         " task")
+            return False
+
+        if self.get_status() != D3RTask.NOTFOUND_STATUS:
+            logger.warning(self.get_name() + " task was already " +
+                           "attempted, but there was a problem")
+            self.set_error(self.get_dir_name() + ' already exists and ' +
+                           'status is ' + self.get_status())
+            return False
+        self._can_run = True
+        return True
+
+    def run(self):
+        """Runs ChimeraProteinLigPrepTask after verifying ChallengeDataTask
+           was good
+
+           Method requires can_run() to be called before hand with
+           successful outcome
+           Otherwise method invokes D3RTask.start then this method
+           creates a directory and invokes blastnfilter script.  Upon
+           completion results are analyzed and success or error status
+           is set appropriately and D3RTask.end is invoked
+           """
+        super(ChimeraProteinLigPrepTask, self).run()
+
+        if self._can_run is False:
+            logger.debug(
+                self.get_dir_name() + ' cannot run cause _can_run flag '
+                                      'is False')
+            return
+
+        try:
+            logger.debug('chimeraprep set to ' +
+                         self.get_args().chimeraprep)
+        except AttributeError:
+            self.set_error('chimeraprep not set')
+            self.end()
+            return
+
+        try:
+            logger.debug('pdbdb set to ' +
+                         self.get_args().pdbdb)
+        except AttributeError:
+            self.set_error('pdbdb not set')
+            self.end()
+            return
+
+        chall = ChallengeDataTask(self._path, self._args)
+
+        #
+        # chimeraprep.py --candidatedir <path to stage.2.blastnfilter> \
+        # --outdir <path to stage.3.proteinligprep>
+        cmd_to_run = (self.get_args().chimeraprep + ' --candidatedir ' +
+                      chall.get_dir() +
+                      ' --pdbdb ' + self.get_args().pdbdb +
+                      ' --outdir ' + self.get_dir())
+
+        chimeraprep_name = os.path.basename(self.get_args().chimeraprep)
+
+        self.run_external_command(chimeraprep_name, cmd_to_run,
+                                  True)
+        # assess the result
+        self.end()
---
+++ tests/celpp/test_chimeraprep.py
@@ -0,0 +1,268 @@
+__author__ = 'churas'
+
+import unittest
+import tempfile
+import os.path
+
+"""
+test_chimeraprep
+--------------------------------
+
+Tests for `chimeraprep` module.
+"""
+
+import shutil
+import os
+from d3r.celpp.task import D3RParameters
+from d3r.celpp.task import D3RTask
+from d3r.celpp.challengedata import ChallengeDataTask
+from d3r.celpp.chimeraprep import ChimeraProteinLigPrepTask
+
+
+class TestChimeraProteinLigPrepTask(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_uploadable_files(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            # try with no dir
+            self.assertEqual(task.get_uploadable_files(), [])
+
+            # try with empty dir
+            task.create_dir()
+            self.assertEqual(task.get_uploadable_files(), [])
+
+            # try with final log
+            final_log = os.path.join(task.get_dir(),
+                                     ChimeraProteinLigPrepTask.FINAL_LOG)
+            open(final_log, 'a').close()
+            flist = task.get_uploadable_files()
+            self.assertEqual(len(flist), 1)
+            flist.index(final_log)
+
+            # add error out files and try with second pbdid folder
+            # with ligand.mae
+            errfile = os.path.join(task.get_dir(),
+                                   'chimera_proteinligprep.py.stderr')
+            open(errfile, 'a').close()
+            outfile = os.path.join(task.get_dir(),
+                                   'chimera_proteinligprep.py.stdout')
+            open(outfile, 'a').close()
+
+
+            flist = task.get_uploadable_files()
+            self.assertEqual(len(flist), 3)
+            flist.index(errfile)
+            flist.index(outfile)
+            flist.index(final_log)
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_can_run(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            # no challengedir task found so it cannot run
+            params = D3RParameters()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            self.assertEqual(task.can_run(), False)
+            self.assertEqual(task.get_error(),
+                             'challengedata task has notfound status')
+
+            # challengedir  running
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.START_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            self.assertEqual(task.can_run(), False)
+            self.assertEqual(task.get_error(),
+                             'challengedata task has start status')
+
+            # challengedata failed
+            error_file = os.path.join(chall.get_dir(),
+                                      D3RTask.ERROR_FILE)
+            open(error_file, 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            self.assertEqual(task.can_run(), False)
+            self.assertEqual(task.get_error(),
+                             'challengedata task has error status')
+
+            # challengedir success
+            os.remove(error_file)
+            open(os.path.join(chall.get_dir(),
+                              D3RTask.COMPLETE_FILE), 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            self.assertEqual(task.can_run(), True)
+            self.assertEqual(task.get_error(), None)
+
+            # chimeraproteinligprep task exists already
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            task.create_dir()
+            self.assertEqual(task.can_run(), False)
+            self.assertEqual(task.get_error(),
+                             task.get_dir_name() +
+                             ' already exists and status is unknown')
+
+            # chimeraproteinligprep already complete
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            open(os.path.join(task.get_dir(),
+                              D3RTask.COMPLETE_FILE), 'a').close()
+            self.assertEqual(task.can_run(), False)
+            self.assertEqual(task.get_error(), None)
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_fails_cause_can_run_is_false(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            # return immediately cause can_run is false
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            task.run()
+            self.assertEqual(task.get_error(),
+                             'challengedata task has notfound status')
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_fails_cause_proteinligprep_not_set(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.COMPLETE_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            task.run()
+            self.assertEqual(task.get_error(),
+                             'chimeraprep not set')
+            # test files get created
+            self.assertEqual(os.path.isdir(task.get_dir()),
+                             True)
+            errfile = os.path.join(task.get_dir(),
+                                   D3RTask.ERROR_FILE)
+            self.assertEqual(os.path.isfile(errfile), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_fails_cause_pdbdb_not_set(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.chimeraprep = 'false'
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.COMPLETE_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+            task.run()
+            self.assertEqual(task.get_error(),
+                             'pdbdb not set')
+            # test files get created
+            self.assertEqual(os.path.isdir(task.get_dir()),
+                             True)
+            errfile = os.path.join(task.get_dir(),
+                                   D3RTask.ERROR_FILE)
+            self.assertEqual(os.path.isfile(errfile), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_fails_cause_chimeraprep_fails(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.chimeraprep = 'false'
+            params.pdbdb = '/foo'
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.COMPLETE_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+
+            task.run()
+            self.assertEqual(task.get_error(),
+                             'Non zero exit code: 1 received. Standard out: ' +
+                             ' Standard error: ')
+            # test file gets created
+            errfile = os.path.join(task.get_dir(),
+                                   D3RTask.ERROR_FILE)
+            self.assertEqual(os.path.isfile(errfile), True)
+
+            stderr = os.path.join(task.get_dir(),
+                                  'false.stderr')
+            self.assertEqual(os.path.isfile(stderr), True)
+            stdout = os.path.join(task.get_dir(),
+                                  'false.stdout')
+            self.assertEqual(os.path.isfile(stdout), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_fails_cause_proteinligprep_is_not_found(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.chimeraprep = '/bin/doesnotexist'
+            params.pdbdb = '/foo'
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.COMPLETE_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+
+            task.run()
+            self.assertEqual(task.get_error(),
+                             'Caught Exception trying to run ' +
+                             '/bin/doesnotexist --candidatedir ' +
+                             chall.get_dir() + ' --pdbdb ' +
+                             '/foo --outdir ' +
+                             task.get_dir() +
+                             ' : [Errno 2] No such file or directory')
+
+            # test files get created
+            errfile = os.path.join(task.get_dir(),
+                                   D3RTask.ERROR_FILE)
+            self.assertEqual(os.path.isfile(errfile), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_succeeds(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            params = D3RParameters()
+            params.chimeraprep = 'true'
+            params.pdbdb = '/foo'
+            chall = ChallengeDataTask(temp_dir, params)
+            chall.create_dir()
+            open(os.path.join(chall.get_dir(), D3RTask.COMPLETE_FILE),
+                 'a').close()
+            task = ChimeraProteinLigPrepTask(temp_dir, params)
+
+            task.run()
+            self.assertEqual(task.get_error(), None)
+            # test files get created
+            errfile = os.path.join(task.get_dir(),
+                                   D3RTask.ERROR_FILE)
+            self.assertEqual(os.path.isfile(errfile), False)
+
+            compfile = os.path.join(task.get_dir(),
+                                    D3RTask.COMPLETE_FILE)
+            self.assertEqual(os.path.isfile(compfile), True)
+            stderr = os.path.join(task.get_dir(),
+                                  'true.stderr')
+            self.assertEqual(os.path.isfile(stderr), True)
+            stdout = os.path.join(task.get_dir(),
+                                  'true.stdout')
+            self.assertEqual(os.path.isfile(stdout), True)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
