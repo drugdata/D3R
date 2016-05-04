c0df20a9f72d8107d176a5c47e77ba24d517c8c7
Chris Churas <churas@gmail.com>
2016-05-04 16:08:57 -0500

    initial revision

---
+++ tests/test_vinadocking.py
@@ -0,0 +1,35 @@
+__author__ = 'churas'
+
+"""
+test_vinadocking
+--------------------------------
+
+Tests for `vinadocking` module.
+"""
+
+import unittest
+import tempfile
+import logging
+import os
+import os.path
+import shutil
+
+from d3r import vinadocking
+
+class TestVinaDocking(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_dock_center_missing_comma(self):
+        center = '1,2'
+        try:
+            vinadocking.dock('ligand', 'protein', center)
+        except IndexError as ie:
+            self.assertEqual(str(ie),'list index out of range',str(ie))
+
+if __name__ == '__main__':
+    unittest.main()
