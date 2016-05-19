5ebd2061ae9d561f4e739756869ddd2545ca0a4d
Jeff Wagner <j5wagner@ucsd.edu>
2016-05-18 18:27:30 -0700

    Small fixes

--- d3r/glidedocking.py
+++ d3r/glidedocking.py
@@ -72,7 +72,7 @@ def main_glide (stage_3_result, stage_4_working, update= True):
             continue
             
         # Get the candidate protein names in this directory
-        candidate_proteins = glob.glob('./*-????_????_prepared.maegz')
+        candidate_proteins = glob.glob('./*-????_????_prepared.mae')
         logging.info('Found candidates %r' %(candidate_proteins))
 
         # Get the ligand names in this directory
--- d3r/vinadocking.py
+++ d3r/vinadocking.py
@@ -53,7 +53,7 @@ def main_vina (stage_3_result, stage_4_working, update= True):
             continue
         
         # Get the candidate protein names in this directory
-        candidate_proteins = glob.glob('./*-????_????_prepared.mae')
+        candidate_proteins = glob.glob('./*-????_????_prepared.mol2')
         
         logging.info('Found candidates %r' %(candidate_proteins))
         # Get the ligand names in this directory
