1c39cd335ec6334288be6202a4b6905b5a12cbb6
Jeff Wagner <j5wagner@ucsd.edu>
2016-05-18 17:20:10 -0700

    Small fixes

--- d3r/vinadocking.py
+++ d3r/vinadocking.py
@@ -53,7 +53,7 @@ def main_vina (stage_3_result, stage_4_working, update= True):
             continue
         
         # Get the candidate protein names in this directory
-        candidate_proteins = glob.glob('./*-????-????_prepared.mol2')
+        candidate_proteins = glob.glob('./*-????_????_prepared.mol2')
         
         # Get the ligand names in this directory
         ligand_mol2s = glob.glob('lig_*_prepped.mol2')
