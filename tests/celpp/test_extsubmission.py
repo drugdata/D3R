db708f22b37d9789e8586a2ee9d7797d5d6f3df7
Chris Churas <churas@gmail.com>
2016-06-24 15:31:14 -0700

    added some tests for factory constructor

--- tests/celpp/test_extsubmission.py
+++ tests/celpp/test_extsubmission.py
@@ -5,10 +5,11 @@ __author__ = 'churas'
 
 import unittest
 import tempfile
-import stat
-import re
-import tarfile
-from mock import Mock
+import shutil
+import os
+
+from d3r.celpp.extsubmission import ExternalDataSubmissionFactory
+from d3r.celpp.task import D3RParameters
 
 
 """
@@ -26,9 +27,43 @@ class TestExternalSubmission(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_externaldatasubmissionfactory_noftpconfig(self):
+        params = D3RParameters()
+        fac = ExternalDataSubmissionFactory('/foo', params)
+        self.assertEqual(fac.get_file_transfer(), None)
+        self.assertEqual(fac._get_challenge_package_results_file_name('xxx'),
+                         'celpp_week0_0_dockedresults_xxx.tar.gz')
 
-    def testfoo(self):
-        self.assertEqual(1, 2)
+    def test_externaldatasubmissionfactory_ftpconfig_set(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            figfile = os.path.join(temp_dir,'foo')
+            f = open(figfile, 'w')
+            f.write('host blah.blah.com\n')
+            f.write('user bob\n')
+            f.write('pass ha\n')
+            f.write('path /celppweekly/ha\n')
+            f.write('challengepath /celppweekly/ha\n')
+            f.flush()
+            f.close()
+            params = D3RParameters()
+            params.ftpconfig = figfile
+            fac = ExternalDataSubmissionFactory('/foo', params)
+            self.assertTrue(fac.get_file_transfer() != None)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_externaldatasubmissionfactory_getter_setter(self):
+        params = D3RParameters()
+        params.ha = 'hi'
+        fac = ExternalDataSubmissionFactory('/foo', params)
+        self.assertEqual(fac.get_file_transfer(), None)
+        self.assertEqual(fac.get_path(), '/foo')
+        self.assertEqual(fac.get_args().ha, 'hi')
+
+        fac.set_file_transfer('yo')
+        self.assertEqual(fac.get_file_transfer(), 'yo')
+
 
 if __name__ == '__main__':
     unittest.main()
