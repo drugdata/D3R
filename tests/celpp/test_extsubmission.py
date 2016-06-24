e855a2f38fa375b377eaa59dc3753eb7838eb301
Chris Churas <churas@gmail.com>
2016-06-24 16:25:35 -0700

    added test for _get_challenge_package_results_file_name

--- tests/celpp/test_extsubmission.py
+++ tests/celpp/test_extsubmission.py
@@ -10,7 +10,7 @@ import os
 
 from d3r.celpp.extsubmission import ExternalDataSubmissionFactory
 from d3r.celpp.task import D3RParameters
-
+from d3r.celpp import util
 
 """
 test_extsubmission
@@ -31,8 +31,6 @@ class TestExternalSubmission(unittest.TestCase):
         params = D3RParameters()
         fac = ExternalDataSubmissionFactory('/foo', params)
         self.assertEqual(fac.get_file_transfer(), None)
-        self.assertEqual(fac._get_challenge_package_results_file_name('xxx'),
-                         'celpp_week0_0_dockedresults_xxx.tar.gz')
 
     def test_externaldatasubmissionfactory_ftpconfig_set(self):
         temp_dir = tempfile.mkdtemp()
@@ -64,6 +62,26 @@ class TestExternalSubmission(unittest.TestCase):
         fac.set_file_transfer('yo')
         self.assertEqual(fac.get_file_transfer(), 'yo')
 
+    def test_get_challenge_package_results_file_name(self):
+        params = D3RParameters()
+
+        # test under non existant dir
+        fac = ExternalDataSubmissionFactory('/foo', params)
+        self.assertEqual(fac._get_challenge_package_results_file_name('xxx'),
+                         'celpp_week0_0_dockedresults_xxx.tar.gz')
+
+        # test under 2016 week 40
+        temp_dir = tempfile.mkdtemp()
+        try:
+            year = os.path.join(temp_dir, '2016')
+            week = os.path.join(year, util.DATA_SET_WEEK_PREFIX + '40')
+            os.makedirs(week)
+            fac = ExternalDataSubmissionFactory(week, params)
+            self.assertEqual(fac._get_challenge_package_results_file_name('xxx'),
+                             'celpp_week40_2016_dockedresults_xxx.tar.gz')
+
+        finally:
+            shutil.rmtree(temp_dir)
 
 if __name__ == '__main__':
     unittest.main()
