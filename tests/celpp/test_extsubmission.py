5f0c777e0101527be8c4f28666f472137c2b859f
Chris Churas <churas@gmail.com>
2016-06-24 13:57:38 -0700

    initial revision

---
+++ tests/celpp/test_extsubmission.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'churas'
+
+import unittest
+import tempfile
+import stat
+import re
+import tarfile
+from mock import Mock
+
+
+"""
+test_extsubmission
+--------------------------------
+
+Tests for `extsubmission` module.
+"""
+
+
+class TestExternalSubmission(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def testfoo(self):
+        self.assertEqual(1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
