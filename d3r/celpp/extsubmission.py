c556526e6fea19cb7827cfc37a05101a682bfa7d
Chris Churas <churas@gmail.com>
2016-06-24 13:57:26 -0700

    added untested implementation for get_external_submissions under ExternalDataSubmissionFactory

--- d3r/celpp/extsubmission.py
+++ d3r/celpp/extsubmission.py
@@ -16,13 +16,22 @@ logger = logging.getLogger(__name__)
 class ExternalDataSubmissionFactory(object):
     """Factory to create ExternalDataSubmissionObjects
     """
+
+    DOCKEDRESULTS = '_dockedresults_'
+
     def __init__(self, path, args):
         """Constructor
         """
-        logger.debug('ftpconfig set to ' + args.ftpconfig)
-        self._file_transfer = FtpFileTransfer(args.ftpconfig)
+        try:
+            logger.debug('ftpconfig set to ' + args.ftpconfig)
+            self._file_transfer = FtpFileTransfer(args.ftpconfig)
+        except Exception:
+            logger.exception('Caught exception')
+            self._file_transfer = None
         self._path = path
         self._args = args
+        ctask = ChallengeDataTask(path, args)
+        self._chall_dir_name = ctask.get_celpp_challenge_data_dir_name()
 
     def get_args(self):
         return self._args
@@ -30,16 +39,56 @@ class ExternalDataSubmissionFactory(object):
     def get_path(self):
         return self._path
 
+    def _get_challenge_package_results_file_name(self, dir_name):
+        return (self._chall_dir_name +
+                ExternalDataSubmissionFactory.DOCKEDRESULTS +
+                dir_name + ChallengeDataTask.TAR_GZ_SUFFIX)
+
     def set_file_transfer(self, filetransfer):
         """Sets file transfer
         """
         self._file_transfer = filetransfer
 
+    def get_file_transfer(self):
+        """Gets file transfer
+        """
+        return self._file_transfer
+
     def _get_submission_dirs(self, remote_dir):
+        """Gets list of directories under `remote_dir`
+        :param remote_dir: path on remote server to examine
+        :returns: list of directory names without path prefix
+        """
+        try:
+            dlist = self._file_transfer.list_dirs(remote_dir)
+            if dlist is None:
+                logger.debug('No directories returned')
+                return []
+            logger.debug('Found ' + str(len(dlist)) + ' directories on ' +
+                         remote_dir)
+            return dlist
+        except Exception:
+            logger.exception('Caught exception')
         return []
 
     def _get_challenge_data_package_file(self, remote_dir, dir_name):
-        return ''
+        ft = self.get_file_transfer()
+        flist = ft.list_files(os.path.normpath(remote_dir, dir_name))
+        if flist is None:
+            logger.info('No files found in ' + dir_name)
+            return None
+        logger.debug('Found ' + str(len(flist)) + ' files in ' + dir_name)
+        chall_fname = self._get_challenge_package_results_file_name(dir_name)
+        logger.debug('Looking for ' + chall_fname + ' file in directory')
+
+        for entry in flist:
+            if entry == chall_fname:
+                logger.debug('Found matching entry')
+                return os.path.normpath(remote_dir, chall_fname)
+
+        # if no matching files were found should we mention any ones
+        # that have a similar name or that have been uploaded since
+        # start of celpp week?
 
     def get_external_data_submissions(self):
         """Generate ExternalDataSubmission objects
@@ -53,14 +102,6 @@ class ExternalDataSubmissionFactory(object):
            the tar.gz file was found otherwise pass None
            along with args.  Append this object to list and
            return it
-        pseudo code:
-           filetransfer = self.get_file_transfer()
-           dlist = filetransfer.list_dirs(ft.get_ftp_remote_submission_dir()
-           for d in dlist:
-             for x in filetransfer.list_files:
-               if x starts with(chall_dir_name) and ends in .tar.gz
-                 # create new external submission object and add to list
-           return #list
         """
         subdir = self._file_transfer.get_ftp_remote_submission_dir()
         dlist = self._get_submission_dirs(subdir)
@@ -71,6 +112,7 @@ class ExternalDataSubmissionFactory(object):
             if chall_file is not None:
                 et = ExternalDataSubmissionTask(self.get_path(), d,
                                                 chall_file, self.get_args())
+                logger.info('Added ExternalData Submission Task: ' + et.get_dir_name())
                 task_list.append(et)
 
         return task_list
@@ -117,9 +159,9 @@ class ExternalDataSubmissionTask(D3RTask):
         pass
 
     def _get_summary_of_docked_results(self):
-        pass
+        return ''
 
-    def _download_external_submission(self):
+    def _download_remote_challenge_data_package(self):
         """Downloads external submissions
            pseudo code:
            ft.download_file(self.get_remote_challenge_data_package())
@@ -143,11 +185,11 @@ class ExternalDataSubmissionTask(D3RTask):
                                       'is False')
             return
         try:
-            pass
-            # self._download_remote_challenge_data_package()
-            # self._untar_challenge_data_package()
-            # summary = self._get_summary_of_docked_results()
-            # self.append_to_email_log(summary)
+
+            self._download_remote_challenge_data_package()
+            self._untar_challenge_data_package()
+            summary = self._get_summary_of_docked_results()
+            self.append_to_email_log(summary)
         except Exception as e:
             logger.exception('Caught exception')
             self.set_error('Caught exception ' + str(e))
