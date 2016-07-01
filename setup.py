5215dd5562947f1560e56c04b5a9bc4b4147ad38
Chris Churas <churas@gmail.com>
2016-07-01 12:48:28 -0700

    bumped version to 1.3.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setup(
     name='d3r',
-    version='1.3.0',
+    version='1.3.1',
     description='Drug Design Data Resource CELPP Runner is an application to run the filtering, docking, and '
                 'evaluation of new sequences from wwpdb',
     long_description=readme + '\n\n' + history,
