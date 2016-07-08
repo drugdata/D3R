375e14e26362587f085acd72847acdd8a77c6419
Chris Churas <churas@gmail.com>
2016-07-08 15:35:19 -0500

    code now uses util.setup_logging and argparse to parse command line arguments

--- d3r/blastnfilter.py
+++ d3r/blastnfilter.py
@@ -3,9 +3,12 @@
 __author__ = 'robswift'
 
 import sys
-from d3r.utilities import io_parser
-from d3r.utilities import run
 import logging
+import argparse
+
+from d3r.utilities import run
+import d3r
+from d3r.celpp import util
 
 # create logger
 logger = logging.getLogger('d3r.blastnfilter')
@@ -17,36 +20,57 @@ class CommandLineParameters(object):
     pass
 
 
-def _setup_logging(loglevel, logformat):
-    """Sets up logging for the application
-
-    Loggers are setup for
-    d3r.blastnfilter
-    d3r.blast.ligand
-
-    :param: loglevel logging level which should be set to a valid input
-            for logging.setLevel()
-    :param: logformat format to set for logging messages
+def _parse_arguments(desc, args):
+    """Parses command line arguments using argparse.
     """
-    logger.setLevel(loglevel)
-    logging.basicConfig(format=logformat)
-    logging.getLogger('d3r.blast.ligand').setLevel(loglevel)
-    logging.getLogger('d3r.blast.hit').setLevel(loglevel)
-    logging.getLogger('d3r.blast.hit_sequence').setLevel(loglevel)
-    logging.getLogger('d3r.blast.query').setLevel(loglevel)
-    logging.getLogger('d3r.filter.filter').setLevel(loglevel)
-    logging.getLogger('d3r.utilities.analysis').setLevel(loglevel)
-    logging.getLogger('d3r.utilities.in_put').setLevel(loglevel)
-    logging.getLogger('d3r.utilities.run').setLevel(loglevel)
+    parsed_arguments = CommandLineParameters()
 
+    help_formatter = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description=desc,
+                                     formatter_class=help_formatter)
+    parser.add_argument("--outdir", required=True, dest="out",
+                        help='Directory to write output to ')
+    parser.add_argument("--nonpolymertsv", required=True,
+                        dest="non_polymer",
+                        help='Nonpolymer tsv file ')
+    parser.add_argument("--sequencetsv", dest="polymer",
+                        required=True,
+                        help='Sequence tsv file ')
+    parser.add_argument("--crystalpH", dest="ph",
+                        required=True,
+                        help='Crystal ph file')
+    parser.add_argument("--pdbblastdb", dest="blast_db",
+                        required=True,
+                        help='Blast database directory')
+    parser.add_argument("--pdbdb", dest="pdb_path",
+                        required=True,
+                        help='PDB Database directory')
+    parser.add_argument("--compinchi", required=True,
+                        help='Components.inchi file')
+    parser.add_argument("--log", dest="loglevel", choices=['DEBUG',
+                        'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help="Set the logging level",
+                        default='WARNING')
+    parser.add_argument('--version', action='version',
+                        version=('%(prog)s ' + d3r.__version__))
+    return parser.parse_args(args, namespace=parsed_arguments)
 
-def main(argv=[__name__]):
+def main(args):
+    desc = """
+              Blastnfilter
+              """
+    options = _parse_arguments(desc, args[1:])
+    options.program = args[0]
+    options.version = d3r.__version__
 
-    itf = io_parser.interface(argv[1:])
-    options = io_parser.SplitInput(itf)
-    _setup_logging(logging.DEBUG, LOG_FORMAT)
+    util.setup_logging(options)
     logger.debug('Starting run ')
-    run.run(options)
+    try:
+        run.run(options)
+    except Exception:
+        logger.exception("Error caught exception")
+        sys.exit(2)
+    sys.exit(0)
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    main(sys.argv)
