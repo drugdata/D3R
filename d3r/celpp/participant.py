68e0c39ad9610983b1f6b040b5dd1c71fda9f717
Chris Churas <churas@gmail.com>
2016-08-23 15:01:39 -0700

    initial revision

---
+++ d3r/celpp/participant.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'churas'
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Participant(object):
+    """Represents a an external participant in the challenge
+    """
+
+    def __init__(self, name, d3rusername, guid, email):
+        """Constructor
+        """
+        self._name = name
+        self._d3rusername = d3rusername
+        self._guid = guid
+        self._email = email
+
+    def get_name(self):
+        """Gets name
+        """
+        return self._name
+
+    def get_d3rusername(self):
+        """Gets d3rusername
+        """
+        return self._d3rusername
+
+    def get_guid(self):
+        """Gets guid
+        """
+        return self._guid
+
+    def get_email(self):
+        """Gets email
+        """
+        return self._email
+
+
+class ParticipantDatabase(object):
+    """Database object that contains a set of `Participants`
+    """
+
+    def __init__(self, participants):
+        """Constructor
+        :param participants: List of participants
+        """
+        self._participants = participants
+
+    def get_participant_by_guid(self, guid):
+        """Searches database for `Participant` with matching `guid`
+        :param guid: guid or unique id for `Participant`
+        :returns `Participant` with matching `guid` or None if not found
+        """
+        if guid is None:
+            logger.warning('guid passed in was None')
+            return None
+
+        if self._participants is None:
+            logger.warning('participants is None')
+            return None
+
+        for p in self._participants:
+            if p.get_guid() == guid:
+                return p
+
+        return None
+
+    def get_participants(self):
+        """Gets participants as list
+        :returns: List of `Participants`
+        """
+        return self._participants
+
+
+class ParticipantDatabaseFromCSVFactory(object):
+    """Factory class that generates `ParticipantDatabase` object from CSV file
+    """
+
+    def __init__(self, csvfile):
+        """Constructor that takes CSV file `csvfile` to generate
+        `ParticipantDatabase` object.  The format of the CSV file
+        is as follows:
+
+        name,d3rusername,guid,email
+        bob smith,bsmith,8675309,bob@bob.com
+
+        :param csvfile: Path to `Participant` CSV file
+        """
+        self._csvfile = csvfile
+
+    def get_participant_database(self):
+        """Parses CSV file set in constructor to create `ParticipantDatabase`
+        :returns `ParticipantDatabase` from CSV file or None if there was an
+        error
+        """
+        if self._csvfile is None:
+            logger.warning('No csv file set')
+            return None
+        try:
+            f = open(self._csvfile, 'r')
+            counter = 0
+            plist = []
+            for line in f:
+                if counter is 0:
+                    if line.startswith('name'):
+                        logger.debug('Skipping header line')
+                        counter = + 1
+                        continue
+
+                splitline = line.rstrip().split(',')
+                if len(splitline) is not 4:
+                    logger.warning('Problems splitting line ' + line +
+                                   ' got ' + str(len(splitline)) +
+                                   ' elements expecting 4')
+                    counter = + 1
+                    continue
+                plist.append(Participant(splitline[0], splitline[1],
+                                         splitline[2], splitline[3]))
+                counter = + 1
+            f.close()
+            return ParticipantDatabase(plist)
+        except Exception as e:
+            logger.exception('Caught exception')
+        return None
---
+++ tests/celpp/test_participant.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'churas'
+
+
+import unittest
+import os
+import tempfile
+import shutil
+
+
+"""
+test_participant
+----------------------------------
+
+Tests for `participant` module.
+"""
+
+from d3r.celpp.participant import Participant
+from d3r.celpp.participant import ParticipantDatabase
+from d3r.celpp.participant import ParticipantDatabaseFromCSVFactory
+
+
+
+
+class TestParticipant(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_participant(self):
+        p = Participant(None, None, None, None)
+        self.assertEqual(p.get_d3rusername(), None)
+        self.assertEqual(p.get_email(), None)
+        self.assertEqual(p.get_guid(), None)
+        self.assertEqual(p.get_name(), None)
+
+        p = Participant('name', 'd3rusername', 'guid', 'email@email.com')
+        self.assertEqual(p.get_d3rusername(), 'd3rusername')
+        self.assertEqual(p.get_email(), 'email@email.com')
+        self.assertEqual(p.get_guid(), 'guid')
+        self.assertEqual(p.get_name(), 'name')
+
+    def test_participant_database(self):
+
+        pdb = ParticipantDatabase(None)
+        self.assertEqual(pdb.get_participants(), None)
+        self.assertEqual(pdb.get_participant_by_guid('bob'), None)
+
+        pdb = ParticipantDatabase([])
+        self.assertEqual(len(pdb.get_participants()), 0)
+        self.assertEqual(pdb.get_participant_by_guid('bob'), None)
+        self.assertEqual(pdb.get_participant_by_guid('guidx'), None)
+        self.assertEqual(pdb.get_participant_by_guid('xguid'), None)
+        self.assertEqual(pdb.get_participant_by_guid('GUID'), None)
+
+        plist = [Participant('1name', '1d3rusername', 'guid',
+                             '1email@email.com'),
+                 Participant('2name', '2d3rusername', 'guidy',
+                             '2email@email.com'),
+                 Participant('3name', '3d3rusername', '12345',
+                             '3email@email.com')]
+
+        pdb = ParticipantDatabase(plist)
+        self.assertEqual(pdb.get_participant_by_guid(None), None)
+        self.assertEqual(pdb.get_participant_by_guid('bob'), None)
+        self.assertEqual(pdb.get_participant_by_guid('guidx'), None)
+        self.assertEqual(pdb.get_participant_by_guid('xguid'), None)
+        self.assertEqual(pdb.get_participant_by_guid('GUID'), None)
+
+        p = pdb.get_participant_by_guid('guid')
+        self.assertEqual(p.get_name(), '1name')
+
+        p = pdb.get_participant_by_guid('guidy')
+        self.assertEqual(p.get_name(), '2name')
+
+        p = pdb.get_participant_by_guid('12345')
+        self.assertEqual(p.get_name(), '3name')
+
+
+    def test_participant_database_from_csv_factory(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+
+            csvfile = os.path.join(temp_dir,'participant_list.csv')
+
+            # parse non existant file
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            self.assertEqual(pfac.get_participant_database(), None)
+
+            # parse empty file
+            open(csvfile, 'a').close()
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            pdb = pfac.get_participant_database()
+            self.assertEqual(len(pdb.get_participants()), 0)
+
+            # parse file with only header
+            f = open(csvfile, 'w')
+            f.write('name,d3rusername,guid,email\n')
+            f.flush()
+            f.close()
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            pdb = pfac.get_participant_database()
+            self.assertEqual(len(pdb.get_participants()), 0)
+
+            # parse file with header and 1 valid entry
+            f = open(csvfile, 'w')
+            f.write('name,d3rusername,guid,email\n')
+            f.write('joe,jj,123,j@j.com\n')
+            f.flush()
+            f.close()
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            pdb = pfac.get_participant_database()
+            self.assertEqual(len(pdb.get_participants()), 1)
+            p = pdb.get_participant_by_guid('123')
+            self.assertEqual(p.get_email(), 'j@j.com')
+
+            # parse file with no header and 1 valid entry
+            f = open(csvfile, 'w')
+            f.write('joe,jj,123,j@j.com\n')
+            f.flush()
+            f.close()
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            pdb = pfac.get_participant_database()
+            self.assertEqual(len(pdb.get_participants()), 1)
+            p = pdb.get_participant_by_guid('123')
+            self.assertEqual(p.get_email(), 'j@j.com')
+
+            # parse file with header and 2 valid and 1 invalid entries
+
+            f = open(csvfile, 'w')
+            f.write('name,d3rusername,guid,email\n')
+            f.write('joe,jj,123,j@j.com\n')
+            f.write('uhoh,2,many,commas,uhoh@email.com\n')
+            f.write('phil,pp,456,p@p.com\n')
+
+            f.flush()
+            f.close()
+            pfac = ParticipantDatabaseFromCSVFactory(csvfile)
+            pdb = pfac.get_participant_database()
+            self.assertEqual(len(pdb.get_participants()), 2)
+            p = pdb.get_participant_by_guid('123')
+            self.assertEqual(p.get_email(), 'j@j.com')
+            p = pdb.get_participant_by_guid('456')
+            self.assertEqual(p.get_email(), 'p@p.com')
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
