dd2f620738695010d95e3ce6c571d8cf22a98afb
Jeff Wagner <j5wagner@nif1.crbs.ucsd.edu>
2016-10-03 10:25:14 -0700

    Added challenge data handler utility

--- d3r/celppade/custom_protein_prep.py
+++ d3r/celppade/custom_protein_prep.py
@@ -9,6 +9,7 @@ import logging
 import time
 import re 
 import shutil 
+from d3r.utilities.challenge_data import ChallengeData
 
 logger = logging.getLogger(__name__)
 
@@ -23,19 +24,6 @@ class ProteinPrep(object):
 
     # This prep script will be required to output files with the appropriate suffixes
     OUTPUT_PROTEIN_SUFFIX = '.pdb'
-    '''
-    OUTPUT_LIG_SUFFIX = '.smi'
-    '''
-
-
-    '''
-    def prepare_ligand(self, lig_smi_file, out_lig_file, info_dic={}):
-       """Does not do any scientific preparation - Passes ligand smiles file forward without any processing 
-        """
-        
-        shutil.copyfile(lig_smi_file, out_lig_file)
-        return True
-    '''
 
     def prepare_protein(self, protein_file, prepared_protein_file, info_dic={}):
         """Does not do any scientific preparation - Passes protein forward without any processing 
@@ -60,14 +48,21 @@ class ProteinPrep(object):
 
     def run_scientific_protein_prep(self, challenge_data_path, pdb_protein_path, working_folder):
         abs_challenge_data_path = os.path.abspath(challenge_data_path)
+        chal_data_obj = ChallengeData(abs_challenge_data_path)
+        if not(chal_data_obj.is_valid_for_celpp):
+            logging.info('%s is not a valid CELPP challenge data directory. Unable to run protein prep.')
+            return False
+        week_chal_data_dict = chal_data_obj.get_targets()
+        week_name = week_chal_data_dict.keys()[0]
+        abs_week_path = os.path.join(abs_challenge_data_path, week_name)
+        pot_target_dirs = week_chal_data_dict[week_name]
         os.chdir(working_folder)
         current_dir_layer_1 = os.getcwd()
 
         ## Get all potential target directories and candidates within
         valid_candidates = {}
-        #target_ligands = {}
-        pot_target_dirs = glob.glob('%s/????' %(abs_challenge_data_path))
-        #target_ids = []
+
+
         # Ensure that the directories are valid
         for pot_target_dir in pot_target_dirs:
             os.chdir(current_dir_layer_1)
@@ -76,28 +71,18 @@ class ProteinPrep(object):
             if len(pot_target_id) != 4:
                 logging.info('Filtering potential target directories: %s is not 4 characters long. Skipping' %(pot_target_id))
                 continue
-            #commands.getoutput('mkdir %s' %(pot_target_id))
-            os.mkdir(pot_target_id)
-            #target_ids.append(pot_target_dir)
-            valid_candidates[pot_target_id] = []
-            target_dir_path = os.path.join(abs_challenge_data_path, pot_target_id)
 
-            # Pull in the ligand inchi/smiles
-            '''
-            lig_smiles_files = glob.glob('%s/lig_*.smi' %(target_dir_path))
-            if len(lig_smiles_files) != 1:
-                logging.info('Unable to find unambiguous ligand smiles for %s - glob returned %r' %(pot_target_id, lig_smiles_files))
-                continue
-            lig_smiles_file = lig_smiles_files[0]
-            local_smiles_file = os.path.basename(lig_smiles_file)
-            dest_smiles_file = os.path.join(pot_target_id, local_smiles_file)
-            commands.getoutput('cp %s %s' %(lig_smiles_file, dest_smiles_file))
-            '''
+            os.mkdir(pot_target_id)
+
+            valid_candidates[pot_target_id] = []
+            target_dir_path = os.path.join(abs_week_path, pot_target_id)
+
+
             center_file = os.path.join(target_dir_path,'center.txt')
             center_file_basename = os.path.basename(center_file)
             center_file_dest = os.path.join(pot_target_id, center_file_basename)
             shutil.copyfile(center_file, center_file_dest)
-            #commands.getoutput('cp %s %s' %(center_file, pot_target_id))
+
 
             # Copy in each valid candidate
             for candidate_file in glob.glob('%s/*-%s_*.pdb' %(target_dir_path, pot_target_id)):
@@ -129,29 +114,11 @@ class ProteinPrep(object):
                 candidate_structure_candidate = parsed_name[0][2]
                 candidate_structure_ligand = parsed_name[0][2]
 
-                # Prepare the ligand
-                '''
-                lig_prefix = smiles_filename.replace('.smi','')
-                prepared_lig_file = '%s_prepared%s' %(lig_prefix, Prep.OUTPUT_LIG_SUFFIX)
-                lig_prep_result = self.prepare_ligand(smiles_filename, prepared_lig_file)
-                if lig_prep_result == False:
-                    logging.info("Unable to prepare the ligand for this target protein: %s. Skipping" %(target_id))
-                    continue 
-                '''
                 # Split the complex 
                 candidate_prefix = '%s-%s_%s' %(candidate_structure_type,
                                                 candidate_structure_target,
                                                 candidate_structure_candidate)
 
-                #split_intermediate_prefix = candidate_prefix+ "_split.pdb"
-                #split_receptor_file = self.split_complex(candidate_filename)
-
-
-                #if not(split_receptor_file):
-                #    logging.info("Unable to split this protein:%s"%(candidate_filename))
-                #    continue
-
-                #logging.info("Successfully split this protein:%s, going to preparation step"%(candidate_filename))
                 prepared_protein_file = "%s_prepared%s" %(candidate_prefix, ProteinPrep.OUTPUT_PROTEIN_SUFFIX)
 
                 preparation_result = self.prepare_protein(candidate_filename, prepared_protein_file)
