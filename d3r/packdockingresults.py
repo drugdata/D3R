49b4963305f1d7ee7facf520bfc813b444990b50
Jeff Wagner <j5wagner@nif1.crbs.ucsd.edu>
2016-10-03 15:56:36 -0700

    Updating getchallengedata, packdockingresults and filetransfer to handle contestant id and upload properly named tarball

--- d3r/packdockingresults.py
+++ d3r/packdockingresults.py
@@ -52,13 +52,25 @@ def make_result_dictionary(dock_dir):
     return result_dic
 
 
-def main_pack_dock_results(dock_dir, pack_dir, ftp_config):
+def main_pack_dock_results(challenge_dir, dock_dir, pack_dir, ftp_config):
     abs_orig_dir = os.getcwd()
+    abs_challenge_dir = os.path.abspath(challenge_dir)
     abs_pack_dir = os.path.abspath(pack_dir)
+
+    ## Parse FTP config if given
     if ftp_config is None:
         abs_ftp_config = None
+        contestant_id = 'XXXXX'
     else:
         abs_ftp_config = os.path.abspath(ftp_config)
+        from d3r.celpp import filetransfer
+        f_f_t_obj = WebDavFileTransfer(abs_ftp_config)
+        contestant_id = f_f_t_obj.get_contestant_id()
+
+    ## Get this week's name
+    chal_data_obj = ChallengeData(abs_challenge_dir)
+    week_name = chal_data_obj.get_week_names()[0]
+
     ## Find all possible uploadable target dirs
     result_dic = make_result_dictionary(dock_dir)
     
@@ -89,7 +101,11 @@ def main_pack_dock_results(dock_dir, pack_dir, ftp_config):
     ## reasonable relative paths)
     os.chdir(abs_pack_dir)
     os.chdir('..')
-    abs_tar_name = abs_pack_dir.rstrip('/') + '.tar.gz'
+    
+    #abs_tar_name = abs_pack_dir.rstrip('/') + '.tar.gz'
+    tar_base_name = week_name + '_dockedresults_' + contestant_id + '.tar.gz'
+    abs_tar_name = os.path.join(abs_pack_dir, tar_name)
+    
     logging.info('Creating tarfile %s in directory %s' %(abs_tar_name, os.getcwd()))
     tarfile_obj = tarfile.open(abs_tar_name, 'w:gz')
     logging.info('Writing to tarfile')
@@ -103,12 +119,14 @@ def main_pack_dock_results(dock_dir, pack_dir, ftp_config):
         logging.info('No ftp_config file given. Skipping upload')
         return
 
-    from d3r.celpp import filetransfer
-    tar_base_name = os.path.basename(abs_tar_name)
-    f_f_t_obj = WebDavFileTransfer(abs_ftp_config)
+    #tar_base_name = os.path.basename(abs_tar_name)
     f_f_t_obj.connect()
+    submission_dir = os.path.join('/dav/',
+                                  #f_f_t_obj.get_remove_submission_dir, 
+                                  contestant_id)
     f_f_t_obj.upload_file_direct(abs_tar_name,
-                                 f_f_t_obj.get_remote_submission_dir(),
+                                 submission_dir
+                                 #f_f_t_obj.get_remote_submission_dir(),
                                  #'/dav/celppweekly/usersubmissions/12345/',
                                  tar_base_name)
     logging.info(f_f_t_obj.get_upload_summary())
@@ -120,12 +138,14 @@ def main_pack_dock_results(dock_dir, pack_dir, ftp_config):
 if ("__main__") == (__name__):
     from argparse import ArgumentParser
     parser = ArgumentParser()
+     parser.add_argument("-c", "--challengedata", metavar="PATH", help = "PATH to the unpacked challenge data package")
     parser.add_argument("-d", "--dockdir", metavar = "PATH", help = "Dir where docking was performed")
     parser.add_argument("-p", "--packdir", metavar="PATH", help = "Dir where the packing and uploading will be performed. Note that, for competition entries, this expects this dir name to already have the proper formatting for this contestant's entry, and will be used as the base of the tar.gz file.")
     parser.add_argument("-f", "--ftpconfig", metavar="PATH", help = "File containing user ftp config information (see included example ftp config for specifics)")
     logger = logging.getLogger()
     logging.basicConfig( format  = '%(asctime)s: %(message)s', datefmt = '%m/%d/%y %I:%M:%S', filename = 'final.log', filemode = 'w', level = logging.INFO )
     args = parser.parse_args()
+    challenge_dir = args.challengedir
     dock_dir = args.dockdir
     pack_dir = args.packdir
     ftp_config = args.ftpconfig
@@ -134,7 +154,7 @@ if ("__main__") == (__name__):
     log_file_path = os.path.join(abs_running_dir, 'final.log')
     log_file_dest = os.path.join(os.path.abspath(pack_dir), 'final.log')
 
-    main_pack_dock_results(dock_dir, pack_dir, ftp_config)
+    main_pack_dock_results(challenge_dir, dock_dir, pack_dir, ftp_config)
 
     #move the final log file to the result dir
     shutil.move(log_file_path, log_file_dest)
