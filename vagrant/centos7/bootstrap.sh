5e21301c171b83838cfb098e7445a0479e2998c8
Chris Churas <churas@gmail.com>
2017-01-25 13:14:52 -0800

    think I got a working puppet apply command

--- vagrant/centos7/bootstrap.sh
+++ vagrant/centos7/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
-echo "Installing git and cloning puppet-d3r"
+echo "Installing git"
 yum install -y git
 
+echo "Cloning puppet-d3r"
 git clone https://github.com/drugdata/puppet-d3r.git d3r
 
 echo "Running puppet apply"
-/opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant/ --catalog /home/vagrant/d3r/metadata.json
+/opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant -e "include d3r"
