e52d0a826bba7fcd962907a1cdf3880bc05033d5
Chris Churas <churas@gmail.com>
2017-01-25 10:58:52 -0800

    added d3r to end of git command to set alternate directory

--- vagrant/centos7/bootstrap.sh
+++ vagrant/centos7/bootstrap.sh
@@ -4,7 +4,7 @@
 echo "Installing git and cloning puppet-d3r"
 yum install -y git
 
-git clone https://github.com/drugdata/puppet-d3r.git
+git clone https://github.com/drugdata/puppet-d3r.git d3r
 
 echo "Running puppet apply"
-/opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant/ --catalog /home/vagrant/puppet-d3r/metadata.json
+/opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant/ --catalog /home/vagrant/d3r/metadata.json
