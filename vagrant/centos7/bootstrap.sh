437be572ec01bd11e7893e891a1ef383cf4f6491
Chris Churas <churas@gmail.com>
2017-01-25 16:23:37 -0800

    pulling from chrisdev branch for testing

--- vagrant/centos7/bootstrap.sh
+++ vagrant/centos7/bootstrap.sh
@@ -5,7 +5,7 @@ echo "Installing git"
 yum install -y git
 
 echo "Cloning puppet-d3r"
-git clone https://github.com/drugdata/puppet-d3r.git d3r
+git clone -b chrisdev https://github.com/drugdata/puppet-d3r.git d3r
 
 echo "Running puppet apply"
 /opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant -e "include d3r"
