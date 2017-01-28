bcfb816b06da01bec31de6834e8dbe3af4b3a628
j-wags <jwagnerjpl@gmail.com>
2017-01-28 14:08:55 -0800

    Update custom_dock.py

--- d3r/celppade/custom_dock.py
+++ d3r/celppade/custom_dock.py
@@ -321,7 +321,7 @@ class Dock(object):
                     tech_prepped_prot_file_list = self.receptor_technical_prep(prot_base_filename, pocket_center, targ_info_dict=targ_info_dict)
                 except:
                     logging.info(sys.exc_info())
-                    logging.info('try/except statement caught error in function receptor_technical_prep.  Skipping candidate %s for target %s.' %(os.path.abspath(prot_base_filename), cand_id, targ_name))
+                    logging.info('try/except statement caught error in function receptor_technical_prep.  Skipping candidate %s for target %s.' %(cand_id, targ_name))
                     continue
 
                 ## Ensure that receptor technical prep was successful
