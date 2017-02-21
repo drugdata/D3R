a7da55c3dfddbd5c05967d51b207c48e9fa8e899
Chris Churas <churas@gmail.com>
2017-02-21 13:16:00 -0800

    switched to using bootstrap to launch puppet

--- vagrant/centos7/bootstrap.sh
+++ vagrant/centos7/bootstrap.sh
@@ -4,7 +4,13 @@
 echo "Installing git and cloning puppet-d3r"
 yum install -y git
 
-git clone https://github.com/drugdata/puppet-d3r.git
+git clone -b chrisdev https://github.com/drugdata/puppet-d3r.git
+
+# append execution command cause I cannot for the life of
+# me get puppet to use the catalog 
+# TODO: fix this hack at some point
+
+echo -e "\n# Run the d3r open class\nclass { 'd3r::open': }\n" >> /home/vagrant/puppet-d3r/manifests/open.pp
 
 echo "Running puppet apply"
-/opt/puppetlabs/bin/puppet apply --modulepath /home/vagrant/ --catalog /home/vagrant/puppet-d3r/metadata.json
+/opt/puppetlabs/bin/puppet apply /home/vagrant/puppet-d3r/manifests/open.pp
