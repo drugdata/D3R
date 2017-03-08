56bbf9a62eaa184eb9c121ef725df6c8e724a695
Chris Churas <churas@gmail.com>
2017-03-08 14:27:27 -0800

    Added post_evaluation.py as an executable script

--- setup.py
+++ setup.py
@@ -75,7 +75,8 @@ setup(
                'd3r/glidedocking.py', 'd3r/evaluate.py',
                'd3r/celppreports.py', 'd3r/vinadocking.py',
                'd3r/genchallengedata.py','d3r/chimera_proteinligprep.py',
-               'd3r/getchallengedata.py','d3r/packdockingresults.py'
+               'd3r/getchallengedata.py','d3r/packdockingresults.py',
+               'd3r/post_evaluation.py'
                ],
     test_suite='tests',
     tests_require=test_requirements
