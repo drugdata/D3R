224f1d7b5e26d48283ad4ba510a7c15894abc167
Chris Churas <churas@gmail.com>
2017-03-15 14:08:26 -0700

    set blastnfiltertimeout to default to 24 hours as per Jeffs suggestion

--- d3r/celpprunner.py
+++ d3r/celpprunner.py
@@ -337,9 +337,9 @@ def _parse_arguments(desc, args):
     parser.add_argument("--blastnfilter", default='blastnfilter.py',
                         help='Path to BlastnFilter script '
                              '(default blastnfilter.py)')
-    parser.add_argument("--blastnfiltertimeout", default=43200, type=int,
+    parser.add_argument("--blastnfiltertimeout", default=86400, type=int,
                         help='Time in seconds script is allowed to run before'
-                             'being killed (default 43200)')
+                             'being killed (default 86400)')
     parser.add_argument("--evaluationtimeout", default=43200, type=int,
                         help='Time in seconds script is allowed to run before'
                              'SIGTERM is sent (default 43200)')
