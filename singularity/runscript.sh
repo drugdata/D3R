c659cd72500d16a218c0d4ea08fb309e77ddc664
Chris Churas <churas@gmail.com>
2017-06-15 14:47:31 -0700

    added LD_LIBRARY_PATH to fix issue where openeye was not working cause wrong libstdc++ library was being loaded

--- singularity/runscript.sh
+++ singularity/runscript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# added cause local libstdc++.so.6 was being loaded instead of one shipped with openeye library
+export LD_LIBRARY_PATH="/usr/lib/python2.7/site-packages/openeye/libs/python2.7-ucs4-linux-x64-g++4.x:$LD_LIBRARY_PATH"
+
+
 exec /usr/bin/celpprunner.py "$@"
