4986ea24fa072d247b24655a2d282b9593da886b
Chris Churas <churas.camera@gmail.com>
2018-05-01 14:12:50 -0700

    set version to 2.5.0 but still had problems so left things commented out

--- singularity/bootstrap.sh
+++ singularity/bootstrap.sh
@@ -2,14 +2,14 @@
 
 echo "Installing base packages"
 yum install -y git epel-release 
-yum install -y wget tar unzip zip gcc xauth
+yum install -y wget tar unzip zip gcc xauth squashfs-tools libarchive-devel
 # yum install -y python-pip 
 # yum install -y python-argparse python-lockfile python-psutil
 #yum install -y python-biopython python-virtualenv python-tox
 #yum install -y pylint python-coverage libXft mesa-* openbabel
 #yum install -y perl-Archive-Tar perl-List-MoreUtils xauth pymol
 
-#VERSION=2.4.2
+#VERSION=2.5.0
 #wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
 #tar xvf singularity-$VERSION.tar.gz
 #cd singularity-$VERSION
